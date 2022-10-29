101c47248306440746e9e86a2c3b618a6e400ae0
Anton Yaitskiy <tonik350@yandex.ru>
2022-10-29 16:48:12 +0700

    second try

--- backend/recipes/management/commands/base.py
+++ backend/recipes/management/commands/base.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
@@ -17,19 +18,24 @@ class CustomCommand(BaseCommand):
 
     def handle(self, *args, **options):
         file_path = os.path.normpath(options["path"])
-
-        with open(file_path, 'r', encoding='utf-8') as file:
-            rows_reader = self.get_rows(file)
-            for line, row in enumerate(rows_reader):
-                try:
-
-                    Ingredient.objects.create(
-                        name=row['name'].capitalize(),
-                        measurement_unit=row['measurement_unit'].lower()
-                    )
-                except IntegrityError as err:
-                    self.stdout.write(f'Line: {line + 1} | Error! {err}')
-            self.stdout.write(f'Файл {file_path} загружен!')
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                rows_reader = self.get_rows(file)
+                for line, row in enumerate(rows_reader):
+                    try:
+                        Ingredient.objects.create(
+                            name=row['name'].capitalize(),
+                            measurement_unit=row['measurement_unit'].lower()
+                        )
+                    except IntegrityError as err:
+                        self.stdout.write(f'Line: {line + 1} | Error! {err}')
+                self.stdout.write(f'Файл {file_path} загружен!')
+        except OSError:
+            print(f'OS-error при попытке открыть {file_path}!')
+            sys.exit(1)
+        except Exception as err:
+            print(f"Unexpected error opening {file_path} is", repr(err))
+            sys.exit(1)
 
     def get_rows(self, file):
         pass
